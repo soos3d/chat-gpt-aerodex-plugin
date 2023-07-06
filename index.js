02415b4754f1f4522c47604940dfee5d6d36475d
Soos3D <99700157+soos3d@users.noreply.github.com>
2023-07-06 18:04:14 -0300

    init

---
+++ index.js
@@ -0,0 +1,194 @@
+// Import required modules
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+// Import custom modules
+const {
+  getAirportData,
+  getMultipleStationsMetar,
+  getMultipleMetarWithTaf,
+  getPirepsNearStation,
+  getPirepsWithinDistance,
+  getSigmets,
+  getAirmets,
+  getDiscussion,
+} = require("./src/app");
+
+// Initialize Express application
+const app = express();
+
+// Set the port number
+const PORT = process.env.PORT || 3000;
+
+// Configure Express to parse JSON
+app.use(bodyParser.json());
+
+// Configure CORS options
+const corsOptions = {
+  origin: "https://chat.openai.com",
+  optionsSuccessStatus: 200, // For compatibility with legacy browsers (IE11, various SmartTVs)
+};
+
+// Use CORS middleware with the specified options
+app.use(cors(corsOptions));
+
+// Define content types for different file extensions
+const contentTypes = {
+  ".json": "application/json",
+  ".yaml": "text/yaml",
+};
+
+// Function to read a file and send its contents as a response
+const readFileAndSend = (filePath, res, contentType) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${err}`);
+      return res.status(500).send("An error occurred while reading the file.");
+    }
+    res.setHeader("Content-Type", contentType);
+    res.send(data);
+  });
+};
+
+// Route to serve the plugin manifest
+app.get("/.well-known/ai-plugin.json", (req, res) => {
+  readFileAndSend(
+    path.join(__dirname, ".well-known/ai-plugin.json"),
+    res,
+    contentTypes[".json"]
+  );
+});
+
+// Route to serve the OpenAPI schema
+app.get("/openapi.yaml", (req, res) => {
+  readFileAndSend(
+    path.join(__dirname, "openapi.yaml"),
+    res,
+    contentTypes[".yaml"]
+  );
+});
+
+// Route to serve the logo image
+app.get("/logo.jpg", (req, res) => {
+  res.sendFile(path.join(__dirname, "logo.png"));
+});
+
+// Route to fetch airport data
+app.post("/airport-data", async (req, res) => {
+  const city = req.body.city;
+  try {
+    const data = await getAirportData(city);
+    res.json(data);
+  } catch (error) {
+    console.error(`Error fetching  data: ${error}`);
+    res.status(500).send("Error occurred while fetching  data");
+  }
+});
+
+// Route to fetch multiple stations metar
+app.post("/multiple-stations-metar", async (req, res) => {
+  console.log("Calling metar only");
+  const stations = req.body.stations;
+  try {
+    const data = await getMultipleStationsMetar(stations);
+    //console.log(data);
+    res.json(data);
+  } catch (error) {
+    console.error(`Error fetching metar data: ${error}`);
+    res.status(500).send("Error occurred while fetching metar data");
+  }
+});
+
+// Route to fetch metar with taf
+app.post("/metar-with-taf", async (req, res) => {
+  console.log("Calling metar with taf");
+  const stations = req.body.stations;
+  try {
+    const data = await getMultipleMetarWithTaf(stations);
+    //console.log(data);
+    res.json(data);
+  } catch (error) {
+    console.error(`Error fetching metar data: ${error}`);
+    res.status(500).send("Error occurred while fetching metar data");
+  }
+});
+
+// Route to fetch pireps based on station
+app.post("/get-pireps-standard", async (req, res) => {
+  console.log("Calling pireps");
+  const station = req.body.station;
+  try {
+    const data = await getPirepsNearStation(station);
+    res.json(data);
+  } catch (error) {
+    console.error(`Error fetching  data: ${error}`);
+    res.status(500).send("Error occurred while fetching pireps");
+  }
+});
+
+// Route to fetch pireps based on station and range
+app.post("/get-pireps-within-range", async (req, res) => {
+  console.log("Calling pireps with range");
+  const station = req.body.station;
+  const range = req.body.range;
+  try {
+    const data = await getPirepsWithinDistance(station, range);
+    res.json(data);
+  } catch (error) {
+    console.error(`Error fetching  data: ${error}`);
+    res.status(500).send("Error occurred while fetching pireps");
+  }
+});
+
+// Route to fetch sigmets
+app.post("/get-sigmet", async (req, res) => {
+  console.log("Calling sigmets");
+  try {
+    const data = await getSigmets();
+    res.json(data);
+  } catch (error) {
+    console.error(`Error fetching  data: ${error}`);
+    res.status(500).send("Error occurred while getting Sigmets");
+  }
+});
+
+// Route to fetch airmets
+app.post("/get-airmet", async (req, res) => {
+  console.log("Calling airmet");
+  try {
+    const data = await getAirmets();
+    res.json(data);
+  } catch (error) {
+    console.error(`Error fetching  data: ${error}`);
+    res.status(500).send("Error occurred while getting Sigmets");
+  }
+});
+
+// Route to fetch forecast discussion
+app.post("/forecast-discussion", async (req, res) => {
+  console.log("Calling discussion");
+  const code = req.body.area;
+  try {
+    const data = await getDiscussion(code);
+    res.json(data);
+  } catch (error) {
+    console.error(`Error fetching  data: ${error}`);
+    res.status(500).send("Error occurred while fetching  data");
+  }
+});
+
+// Default route for unimplemented methods and paths
+app.all("/*", (req, res) => {
+  res
+    .status(501)
+    .send(`Method ${req.method} not implemented for path ${req.path}`);
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ src/app.js
@@ -0,0 +1,189 @@
+// Import required modules
+const dotenv = require("dotenv");
+const axios = require("axios");
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Retrieve API key from environment variables
+const API_KEY = process.env.AIRPORTS_API_KEY;
+
+/**
+ * Fetches airport data for a given city using the API Ninjas Airports API.
+ *
+ * @param {string} city - The city for which to fetch airport data.
+ * @returns {Promise<Object>} The airport data for the specified city.
+ * @throws {Error} If an error occurs during the API request.
+ */
+async function getAirportData(city) {
+  const options = {
+    method: "GET",
+    url: `https://api.api-ninjas.com/v1/airports?name=${city}`,
+    headers: {
+      "X-Api-Key": API_KEY,
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(
+      `An error occurred while fetching airport data: ${error.response.data}`
+    );
+    throw error;
+  }
+}
+
+const BASE_URL = "https://beta.aviationweather.gov";
+
+async function getMultipleStationsMetar(airportCodes) {
+  try {
+    const ids = airportCodes.join(",");
+    const response = await axios.get(
+      `${BASE_URL}/cgi-bin/data/metar.php?ids=${ids}&format=decoded`
+    );
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(`An error occurred while fetching weather data: ${error}`);
+    throw error;
+  }
+}
+
+async function getMultipleMetarWithTaf(airportCodes) {
+  try {
+    const ids = airportCodes.join(",");
+    const [metarResponse, tafResponse] = await Promise.all([
+      axios.get(`${BASE_URL}/cgi-bin/data/metar.php?ids=${ids}&format=decoded`),
+      axios.get(`${BASE_URL}/cgi-bin/data/metar.php?ids=${ids}&taf=on`),
+    ]);
+
+    // Split the taf response into an array at each newline, remove the first element, and join it back into a string
+    const cleanedTaf = tafResponse.data.split("\n").slice(1).join("\n");
+
+    // Combine the responses
+    const combinedResponse = {
+      metar: metarResponse.data,
+      taf: cleanedTaf,
+    };
+
+    console.log(combinedResponse);
+    return combinedResponse;
+  } catch (error) {
+    console.error(`An error occurred while fetching weather data: ${error}`);
+    throw error;
+  }
+}
+
+async function getPirepsNearStation(airportCode) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/cgi-bin/data/pirep.php?id=${airportCode}&format=decoded`
+    );
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(`An error occurred while fetching weather data: ${error}`);
+    throw error;
+  }
+}
+
+async function getPirepsWithinDistance(airportCode, range) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/cgi-bin/data/pirep.php?id=${airportCode}&format=decoded&distance=${range}`
+    );
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(`An error occurred while fetching weather data: ${error}`);
+    throw error;
+  }
+}
+
+function parseWeatherMets(weatherMetsString) {
+  const splitIndex = weatherMetsString.indexOf("AIRMET for");
+
+  const sigmetString = weatherMetsString.slice(0, splitIndex);
+  const airmetString = weatherMetsString.slice(splitIndex);
+
+  const sigmets = sigmetString
+    ? sigmetString
+        .trim()
+        .split("SIGMET for")
+        .slice(1)
+        .map((s) => "SIGMET for" + s)
+    : [];
+  const airmets = airmetString
+    ? airmetString
+        .trim()
+        .split("AIRMET for")
+        .slice(1)
+        .map((a) => "AIRMET for" + a)
+    : [];
+
+  return { sigmets, airmets };
+}
+
+async function getSigmets() {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/cgi-bin/data/airsigmet.php?format=decoded`
+    );
+
+    const { sigmets, airmets } = parseWeatherMets(response.data);
+    console.log("SIGMETs:", sigmets);
+    const result = { sigmets: sigmets };
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(`An error occurred while fetching weather data: ${error}`);
+    throw error;
+  }
+}
+
+async function getAirmets() {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/cgi-bin/data/airsigmet.php?format=decoded`
+    );
+
+    const { sigmets, airmets } = parseWeatherMets(response.data);
+    console.log("AIRMETSs:", airmets);
+    const result = { airmets: airmets };
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(`An error occurred while fetching weather data: ${error}`);
+    throw error;
+  }
+}
+
+async function getDiscussion(code) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/cgi-bin/data/fcstdisc.php?cwa=k${code}`
+    );
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(`An error occurred while fetching weather data: ${error}`);
+    throw error;
+  }
+}
+
+module.exports = {
+  getAirportData,
+  getMultipleStationsMetar,
+  getMultipleMetarWithTaf,
+  getPirepsNearStation,
+  getPirepsWithinDistance,
+  getSigmets,
+  getAirmets,
+  getDiscussion,
+};
