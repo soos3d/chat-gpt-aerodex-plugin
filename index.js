039f13468a50043ee1172fb14e9c5f62e606da62
Soos3D <99700157+soos3d@users.noreply.github.com>
2023-08-02 19:51:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -86,36 +86,37 @@ app.use((req, res, next) => {
   next();
 });
 
-// Middleware to capture the response body
-app.use((req, res, next) => {
-  var oldWrite = res.write,
-    oldEnd = res.end;
-
-  var chunks = [];
-
-  res.write = function (chunk) {
-    chunks.push(chunk);
-
-    oldWrite.apply(res, arguments);
-  };
-
-  res.end = function (chunk) {
-    if (chunk) chunks.push(chunk);
-
-    var body = Buffer.concat(chunks);
-    if (
-      res.getHeader("Content-Type").includes("text") ||
-      res.getHeader("Content-Type").includes("json")
-    ) {
-      body = body.toString("utf8");
-      console.log(req.path, body);
-      res.body = body; // Here's the body
-    }
-
-    oldEnd.apply(res, arguments);
-  };
-
+// Middleware to log requests and responses
+app.use(async (req, res, next) => {
+  const start = Date.now();
   next();
+  const ms = Date.now() - start;
+
+  // Skip logging for certain routes
+  const excludedRoutes = [
+    "/logo",
+    "/openapi.yaml",
+    "/.well-known/ai-plugin.json",
+  ];
+  if (excludedRoutes.includes(req.path)) {
+    return;
+  }
+
+  const log = {
+    method: req.method,
+    url: req.url,
+    status: res.statusCode,
+    length: res.get("Content-Length"),
+    responseTime: ms,
+    requestBody: req.body,
+    responseBody: res.body,
+    timestamp: new Date(),
+  };
+
+  await withDbConnection(async () => {
+    const db = mongoClient.db("AeroDex_plugin"); // replace with your database name
+    await db.collection("logs").insertOne(log);
+  });
 });
 
 // Define content types for different file extensions
